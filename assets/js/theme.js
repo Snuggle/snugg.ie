3df2a94daae00e2895f281655894ac0b7488b175
Evie Snuggle <^-^@Snugg.ie>
2020-05-31 16:15:33 +0100

    Add dark theme to main page!

---
+++ assets/js/theme.js
@@ -0,0 +1,42 @@
+function set_theme() {
+  if (localStorage['theme']) {
+    document.getElementById('main').classList.add('force-theme-'+localStorage['theme']);
+    document.getElementById('nav').classList.add('force-theme-'+localStorage['theme']);
+    document.querySelector('body').classList.add('force-theme-'+localStorage['theme']);
+  }
+}
+
+function reset_theme() {
+  localStorage.removeItem('theme');
+  document.getElementById('main').classList.add('animate-theme-change');
+  document.getElementById('main').classList.forEach(className => {
+    if (className.startsWith('force-theme-')) {
+        document.getElementById('main').classList.remove(className);
+    }
+  });
+
+  localStorage.removeItem('theme');
+  document.getElementById('nav').classList.add('animate-theme-change');
+  document.getElementById('nav').classList.forEach(className => {
+    if (className.startsWith('force-theme-')) {
+        document.getElementById('nav').classList.remove(className);
+    }
+  });
+
+  localStorage.removeItem('theme');
+  document.querySelector('body').classList.add('animate-theme-change');
+  document.querySelector('body').classList.forEach(className => {
+    if (className.startsWith('force-theme-')) {
+        document.querySelector('body').classList.remove(className);
+    }
+  });
+
+}
+
+function toggle_theme(theme) {
+  reset_theme();
+  localStorage['theme'] = theme;
+  set_theme();
+}
+
+set_theme();
