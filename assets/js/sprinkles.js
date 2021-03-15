dc02cd32c985392e22d5597b39a1c16f7f132a10
Evie Snuggle <^-^@Snugg.ie>
2021-03-15 17:21:35 +0000

    🎨 Beautify JavaScript.

--- assets/js/sprinkles.js
+++ assets/js/sprinkles.js
@@ -1,80 +1,78 @@
-
 /*!
-* Dynamically changing favicons with JavaScript
-* Works in all A-grade browsers except Safari and Internet Explorer
-* Demo: http://mathiasbynens.be/demo/dynamic-favicons
-*/
+ * Dynamically changing favicons with JavaScript
+ * Works in all A-grade browsers except Safari and Internet Explorer
+ * Demo: http://mathiasbynens.be/demo/dynamic-favicons
+ */
 
 // HTML5™, baby! http://mathiasbynens.be/notes/document-head
 document.head = document.head || document.getElementsByTagName('head')[0];
 
 function changeFavicon(src) {
-    var link = document.createElement('link'),
-        oldLink = document.getElementById('dynamic-favicon');
-    link.id = 'dynamic-favicon';
-    link.rel = 'shortcut icon';
-    link.href = src;
-    if (oldLink) {
-    document.head.removeChild(oldLink);
-    }
-    document.head.appendChild(link);
+	var link = document.createElement('link'),
+		oldLink = document.getElementById('dynamic-favicon');
+	link.id = 'dynamic-favicon';
+	link.rel = 'shortcut icon';
+	link.href = src;
+	if (oldLink) {
+		document.head.removeChild(oldLink);
+	}
+	document.head.appendChild(link);
 }
 
 function changeLogo(path) {
-    document.getElementById("main-logo").src = path; 
-    changeFavicon(path)
+	document.getElementById("main-logo").src = path;
+	changeFavicon(path)
 }
 
 // Return the southern hemisphere season for a date
 // If no date provided, uses current system date
 function getSeason(d) {
-    d = d || new Date();
-    var mon = d.getMonth() + 1;  // Adjust to be indexed from 1
-    var day = d.getDate();
-    var seasons = ['summer','autumn','winter','spring'];
+	d = d || new Date();
+	var mon = d.getMonth() + 1; // Adjust to be indexed from 1
+	var day = d.getDate();
+	var seasons = ['summer', 'autumn', 'winter', 'spring'];
 
-    // Do silly seasons here
-    if (mon == 2) {
-        if (day >= 0 && day <= 10) {
-            changeLogo("/assets/images/sprinkles/vector/sprinkles-me.svg"); 
-            return 'my season';
-        }
-    }
-    if (mon == 4) {
-        if (day >= 0 && day <= 10) {
-            changeLogo("/assets/images/sprinkles/vector/sprinkles-easter.svg"); 
-            return 'easter season';
-        }
-    }
-    if (mon == 10) {
-        if (day == 31) {
-            changeLogo("/assets/images/sprinkles/vector/sprinkles-halloween-ghost.svg"); 
-            return 'halloween'; 
-        }
-        if (day >= 13 && day <= 30) {
-            changeLogo("/assets/images/sprinkles/vector/sprinkles-halloween.svg"); 
-            return 'halloween season';
-        }
-    }
-    if (mon == 12) {
-        if (day >= 13 && day <= 27) {
-            changeLogo("/assets/images/sprinkles/vector/sprinkles-christmas.svg"); 
-            return 'xmas season';
-        }
-        if (day >= 28 && day <= 31) {
-            return 'nye';
-        }
-    }
+	// Do silly seasons here
+	if (mon == 2) {
+		if (day >= 0 && day <= 10) {
+			changeLogo("/assets/images/sprinkles/vector/sprinkles-me.svg");
+			return 'my season';
+		}
+	}
+	if (mon == 4) {
+		if (day >= 0 && day <= 10) {
+			changeLogo("/assets/images/sprinkles/vector/sprinkles-easter.svg");
+			return 'easter season';
+		}
+	}
+	if (mon == 10) {
+		if (day == 31) {
+			changeLogo("/assets/images/sprinkles/vector/sprinkles-halloween-ghost.svg");
+			return 'halloween';
+		}
+		if (day >= 13 && day <= 30) {
+			changeLogo("/assets/images/sprinkles/vector/sprinkles-halloween.svg");
+			return 'halloween season';
+		}
+	}
+	if (mon == 12) {
+		if (day >= 13 && day <= 27) {
+			changeLogo("/assets/images/sprinkles/vector/sprinkles-christmas.svg");
+			return 'xmas season';
+		}
+		if (day >= 28 && day <= 31) {
+			return 'nye';
+		}
+	}
 
-    // If wasn't a silly season, do others
-    mon = Math.floor((mon % 12) / 3);
-    return seasons[mon];
+	// If wasn't a silly season, do others
+	mon = Math.floor((mon % 12) / 3);
+	return seasons[mon];
 }
 
 console.log('Currently it is ' + getSeason()); // Currently it is summer
 
 
-$(document).ready(function(){
-    document.body.classList.add(getSeason());
-  });
-  
+$(document).ready(function() {
+	document.body.classList.add(getSeason());
+});
--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,47 +1,48 @@
 function set_theme() {
 	if (localStorage['theme']) {
-	  document.getElementById('main').classList.add('force-theme-'+localStorage['theme']);
-	  document.body.classList.add('force-theme-'+localStorage['theme']);
-	  if(document.getElementById("nav")){
-	  document.getElementById('nav').classList.add('force-theme-'+localStorage['theme']);
-	  }}
-  
+		document.getElementById('main').classList.add('force-theme-' + localStorage['theme']);
+		document.body.classList.add('force-theme-' + localStorage['theme']);
+		if (document.getElementById("nav")) {
+			document.getElementById('nav').classList.add('force-theme-' + localStorage['theme']);
+		}
+	}
+
 	//   if (localStorage['theme'] === 'dark') {
 	// 	  document.getElementById('avatar').src='https://github.com/Snuggle.png'
 	//   } else {
 	// 	  document.getElementById('avatar').src='https://github.com/Snuggle.png'
 	//   }
-  }
-  
-  function reset_theme() {
+}
+
+function reset_theme() {
 	localStorage.removeItem('theme');
 	document.getElementById('main').classList.forEach(className => {
-	  if (className.startsWith('force-theme-')) {
-		  document.getElementById('main').classList.remove(className);
-	  }
+		if (className.startsWith('force-theme-')) {
+			document.getElementById('main').classList.remove(className);
+		}
 	});
-  
-	  localStorage.removeItem('theme');
-	  document.querySelector('body').classList.forEach(className => {
-		  if (className.startsWith('force-theme-')) {
-				  document.querySelector('body').classList.remove(className);
-		  }
-	  });
-  
-	  if(document.getElementById("nav")){
-		  localStorage.removeItem('theme');
+
+	localStorage.removeItem('theme');
+	document.querySelector('body').classList.forEach(className => {
+		if (className.startsWith('force-theme-')) {
+			document.querySelector('body').classList.remove(className);
+		}
+	});
+
+	if (document.getElementById("nav")) {
+		localStorage.removeItem('theme');
 		document.querySelector('nav').classList.forEach(className => {
-			  document.getElementById('nav').classList.remove(className);
+			document.getElementById('nav').classList.remove(className);
 		});
-	  }
-  
-  
-  }
-  
-  function toggle_theme(theme) {
+	}
+
+
+}
+
+function toggle_theme(theme) {
 	reset_theme();
 	localStorage['theme'] = theme;
 	set_theme();
-  }
-  
-  set_theme();
+}
+
+set_theme();
